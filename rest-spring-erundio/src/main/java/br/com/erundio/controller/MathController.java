d398e15e1da334d1c8166c936286b516fee4556d
Ives Chiba <ivesjundi@gmail.com>
2024-04-11 14:29:16 -0300

    feat :sparkles: sum, sub, mult, div, avg, sqrRoot methods added

--- rest-spring-erundio/src/main/java/br/com/erundio/controller/MathController.java
+++ rest-spring-erundio/src/main/java/br/com/erundio/controller/MathController.java
@@ -20,6 +20,60 @@ public class MathController {
         }
         return convertToDouble(num1) + convertToDouble(num2);
     }
+    @RequestMapping(value = "/subtract/{num1}/{num2}", method = RequestMethod.GET)
+    public Double subtract(
+            @PathVariable(value = "num1") String num1,
+            @PathVariable(value = "num2") String num2) throws Exception{
+
+        if (!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedOperationException("Set a numeric value");
+        }
+        return convertToDouble(num1) - convertToDouble(num2);
+    }
+    @RequestMapping(value = "/mult/{num1}/{num2}", method = RequestMethod.GET)
+    public Double mult(
+            @PathVariable(value = "num1") String num1,
+            @PathVariable(value = "num2") String num2) throws Exception{
+
+        if (!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedOperationException("Set a numeric value");
+        }
+        return convertToDouble(num1) * convertToDouble(num2);
+    }
+
+    @RequestMapping("/div/{num1}/{num2}")
+    public Double div(
+            @PathVariable(value = "num1") String num1,
+            @PathVariable(value = "num2") String num2) throws Exception{
+
+        if (!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedOperationException("Set a numeric value");
+        }
+        return convertToDouble(num1) / convertToDouble(num2);
+    }
+
+    @RequestMapping("/avg/{num1}/{num2}")
+    public Double avg(
+            @PathVariable(value = "num1") String num1,
+            @PathVariable(value = "num2") String num2) throws Exception{
+
+        if (!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedOperationException("Set a numeric value");
+        }
+        return (convertToDouble(num1) + convertToDouble(num2)) / 2;
+    }
+
+    @RequestMapping("/sqrRoot/{num1}")
+    public Double sqrRoot(
+            @PathVariable(value = "num1") String num1) throws Exception{
+
+        if (!isNumeric(num1)){
+            throw new UnsupportedOperationException("Set a numeric value");
+        }
+        return Math.sqrt(convertToDouble(num1));
+    }
+
+
 
     private Double convertToDouble(String strNumber) {
         if (strNumber == null){
