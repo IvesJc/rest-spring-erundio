5cd0743ffab66323251ef7001e843fd0df4f706f
Ives Chiba <ivesjundi@gmail.com>
2024-04-11 14:47:27 -0300

    refactor :recycle: methods moved to service package

--- rest-spring-erundio/src/main/java/br/com/erundio/controller/MathController.java
+++ rest-spring-erundio/src/main/java/br/com/erundio/controller/MathController.java
@@ -1,5 +1,7 @@
 package br.com.erundio.controller;
 
+import br.com.erundio.service.MathService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -10,35 +12,39 @@ public class MathController {
 
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private MathService mathService;
+
     @RequestMapping(value = "/sum/{num1}/{num2}", method = RequestMethod.GET)
     public Double sum(
             @PathVariable(value = "num1") String num1,
             @PathVariable(value = "num2") String num2) throws Exception{
 
-        if (!isNumeric(num1) || !isNumeric(num2)){
+        if (!mathService.isNumeric(num1) || !mathService.isNumeric(num2)){
             throw new UnsupportedOperationException("Set a numeric value");
         }
-        return convertToDouble(num1) + convertToDouble(num2);
+        return mathService.sum(num1, num2);
     }
-    @RequestMapping(value = "/subtract/{num1}/{num2}", method = RequestMethod.GET)
-    public Double subtract(
+    @RequestMapping(value = "/sub/{num1}/{num2}", method = RequestMethod.GET)
+    public Double sub(
             @PathVariable(value = "num1") String num1,
             @PathVariable(value = "num2") String num2) throws Exception{
 
-        if (!isNumeric(num1) || !isNumeric(num2)){
+        if (!mathService.isNumeric(num1) || !mathService.isNumeric(num2)){
             throw new UnsupportedOperationException("Set a numeric value");
         }
-        return convertToDouble(num1) - convertToDouble(num2);
+        return mathService.sub(num1, num2);
     }
     @RequestMapping(value = "/mult/{num1}/{num2}", method = RequestMethod.GET)
     public Double mult(
             @PathVariable(value = "num1") String num1,
             @PathVariable(value = "num2") String num2) throws Exception{
 
-        if (!isNumeric(num1) || !isNumeric(num2)){
+        if (!mathService.isNumeric(num1) || !mathService.isNumeric(num2)){
             throw new UnsupportedOperationException("Set a numeric value");
         }
-        return convertToDouble(num1) * convertToDouble(num2);
+        return mathService.mult(num1, num2);
+
     }
 
     @RequestMapping("/div/{num1}/{num2}")
@@ -46,53 +52,38 @@ public class MathController {
             @PathVariable(value = "num1") String num1,
             @PathVariable(value = "num2") String num2) throws Exception{
 
-        if (!isNumeric(num1) || !isNumeric(num2)){
+        if (!mathService.isNumeric(num1) || !mathService.isNumeric(num2)){
             throw new UnsupportedOperationException("Set a numeric value");
         }
-        return convertToDouble(num1) / convertToDouble(num2);
+        return mathService.div(num1, num2);
+
     }
 
     @RequestMapping("/avg/{num1}/{num2}")
     public Double avg(
             @PathVariable(value = "num1") String num1,
             @PathVariable(value = "num2") String num2) throws Exception{
-
-        if (!isNumeric(num1) || !isNumeric(num2)){
+        if (!mathService.isNumeric(num1) || !mathService.isNumeric(num2)){
             throw new UnsupportedOperationException("Set a numeric value");
         }
-        return (convertToDouble(num1) + convertToDouble(num2)) / 2;
+        return mathService.avg(num1, num2);
+
     }
 
     @RequestMapping("/sqrRoot/{num1}")
     public Double sqrRoot(
             @PathVariable(value = "num1") String num1) throws Exception{
 
-        if (!isNumeric(num1)){
+        if (!mathService.isNumeric(num1)){
             throw new UnsupportedOperationException("Set a numeric value");
         }
-        return Math.sqrt(convertToDouble(num1));
+        return mathService.sqrRoot(num1);
+
     }
 
 
 
-    private Double convertToDouble(String strNumber) {
-        if (strNumber == null){
-            return 0D;
-        }
-        String number = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)){
-            return Double.parseDouble(number);
-        }
-        return 0D;
-    }
 
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null){
-            return false;
-        }
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[+-]?[0-9]*\\.?[0-9]+");
-    }
 
 
 }
---
+++ rest-spring-erundio/src/main/java/br/com/erundio/service/MathService.java
@@ -0,0 +1,51 @@
+package br.com.erundio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MathService {
+
+
+
+
+    public Double convertToDouble(String strNumber) {
+        if (strNumber == null){
+            return 0D;
+        }
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)){
+            return Double.parseDouble(number);
+        }
+        return 0D;
+    }
+
+    public boolean isNumeric(String strNumber) {
+        if (strNumber == null){
+            return false;
+        }
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[+-]?[0-9]*\\.?[0-9]+");
+    }
+
+    public Double sum(String num1, String  num2){
+        return convertToDouble(num1) + convertToDouble(num2);
+    }
+
+    public Double sub(String num1, String  num2){
+        return convertToDouble(num1) - convertToDouble(num2);
+    }
+    public Double mult(String num1, String  num2){
+        return convertToDouble(num1) * convertToDouble(num2);
+    }
+    public Double div(String num1, String  num2){
+        return convertToDouble(num1) / convertToDouble(num2);
+    }
+
+    public Double avg(String num1, String  num2){
+        return (convertToDouble(num1) + convertToDouble(num2)) / 2;
+    }
+
+    public Double sqrRoot(String num1){
+        return Math.sqrt(convertToDouble(num1));
+    }
+}
