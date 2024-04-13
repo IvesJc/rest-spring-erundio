0ccb3c2630058f584a692b3d9f0ce80c51c6f662
Ives Chiba <ivesjundi@gmail.com>
2024-04-13 12:39:23 -0300

    feat :sparkles: person class as entity and person repository created

--- rest-spring-erundio/src/main/java/br/com/erundio/model/Person.java
+++ rest-spring-erundio/src/main/java/br/com/erundio/model/Person.java
@@ -1,14 +1,25 @@
 package br.com.erundio.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+    @Column(name = "last_name",nullable = false, length = 80)
     private String lastName;
+    @Column(nullable = false, length = 100)
     private String adress;
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {
---
+++ rest-spring-erundio/src/main/java/br/com/erundio/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.erundio.repositories;
+
+import br.com.erundio.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
