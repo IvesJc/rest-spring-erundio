5d1052ac2efd4c02e454d7cd7af934b7eea069e0
Ives Chiba <ivesjundi@gmail.com>
2024-04-13 11:31:40 -0300

    feat :sparkles: Person entity created

--- rest-spring-erundio/src/main/java/br/com/erundio/model/Person.java
+++ rest-spring-erundio/src/main/java/br/com/erundio/model/Person.java
@@ -1,10 +1,69 @@
 package br.com.erundio.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Person implements Serializable {
 
     private Long id;
-    private S
+    private String firstName;
+    private String lastName;
+    private String adress;
+    private String gender;
 
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
