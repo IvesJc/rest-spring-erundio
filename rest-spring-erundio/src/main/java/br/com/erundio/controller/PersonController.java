be23aa3ca1aea367eb337b95727a7e4203f1e114
Ives Chiba <ivesjundi@gmail.com>
2024-04-13 14:45:40 -0300

    refactor :recycle: PersonController refactored

--- rest-spring-erundio/src/main/java/br/com/erundio/controller/PersonController.java
+++ rest-spring-erundio/src/main/java/br/com/erundio/controller/PersonController.java
@@ -4,6 +4,7 @@ import br.com.erundio.model.Person;
 import br.com.erundio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +16,28 @@ public class PersonController {
     @Autowired
     private PersonService personServices;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<Person> findAll(){
         return personServices.findAll();
     }
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public Person findById(@PathVariable(value = "id") Long id){
         return personServices.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public Person create(@RequestBody Person person){
         return personServices.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public Person update(@RequestBody Person person){
         return personServices.update(person);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void deleteById(@PathVariable(value = "id") Long id){
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable(value = "id") Long id){
         personServices.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
