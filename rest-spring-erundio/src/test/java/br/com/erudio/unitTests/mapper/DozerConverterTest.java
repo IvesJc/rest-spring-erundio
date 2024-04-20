9dda8fc48cb428e7a18bda16a947d97d2b8fba62
Ives Chiba <ivesjundi@gmail.com>
2024-04-20 14:21:43 -0300

    feat :sparkles: unit tests

---
+++ rest-spring-erundio/src/test/java/br/com/erudio/unitTests/mapper/DozerConverterTest.java
@@ -0,0 +1,99 @@
+package br.com.erudio.unitTests.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import br.com.erudio.dto.PersonDTO;
+import br.com.erudio.dto.mapper.ModelMapper;
+import br.com.erudio.unitTests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.erudio.model.Person;
+
+public class DozerConverterTest {
+    
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToVOTest() {
+        PersonDTO output = ModelMapper.parseObject(inputObject.mockEntity(), PersonDTO.class);
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAdress());
+        assertEquals("Male", output.getGender());
+    }
+
+    @Test
+    public void parseEntityListToVOListTest() {
+        List<PersonDTO> outputList = ModelMapper.parseListObjects(inputObject.mockEntityList(), PersonDTO.class);
+        PersonDTO outputZero = outputList.get(0);
+
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAdress());
+        assertEquals("Male", outputZero.getGender());
+
+        PersonDTO outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAdress());
+        assertEquals("Female", outputSeven.getGender());
+
+        PersonDTO outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAdress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+
+    @Test
+    public void parseVOToEntityTest() {
+        Person output = ModelMapper.parseObject(inputObject.mockVO(), Person.class);
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAdress());
+        assertEquals("Male", output.getGender());
+    }
+
+    @Test
+    public void parserVOListToEntityListTest() {
+        List<Person> outputList = ModelMapper.parseListObjects(inputObject.mockVOList(), Person.class);
+        Person outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAdress());
+        assertEquals("Male", outputZero.getGender());
+        
+        Person outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAdress());
+        assertEquals("Female", outputSeven.getGender());
+        
+        Person outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAdress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+}
