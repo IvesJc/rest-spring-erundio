ab9db8f00b12a7aef926d5b598b49ed6c4c2efd9
Ives Chiba <ivesjundi@gmail.com>
2024-04-20 14:22:24 -0300

    refactor :recycle: dto class instead of record

--- rest-spring-erundio/src/main/java/br/com/erudio/dto/PersonDTO.java
+++ rest-spring-erundio/src/main/java/br/com/erudio/dto/PersonDTO.java
@@ -2,18 +2,62 @@ package br.com.erudio.dto;
 
 import br.com.erudio.model.Person;
 
-public record PersonDTO(
-        Long id,
-        String firstName,
-        String lastName,
-        String adress,
-        String gender) {
+public class PersonDTO{
 
-//    public PersonDTO(Person person) {
-//        this(person.getId(),
-//            person.getFirstName(),
-//            person.getLastName(),
-//            person.getAdress(),
-//            person.getGender());
-//    }
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String adress;
+    private String gender;
+
+    public PersonDTO() {
+    }
+
+    public PersonDTO(Long id, String firstName, String lastName, String adress, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
