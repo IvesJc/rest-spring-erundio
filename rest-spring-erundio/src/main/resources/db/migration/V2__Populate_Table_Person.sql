daaad3d5e95d562574d882746d1ebc8442b267f6
Ives Chiba <ivesjundi@gmail.com>
2024-04-20 23:25:03 -0300

    feat :sparkles: V1 (create) and V2 (insert) added

---
+++ rest-spring-erundio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `adress`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+    (3, 'São Paulo - São Paulo - Brasil', 'Ives', 'Male', 'Chiba'),
+    (4, 'Porbandar - Índia', 'Mahatma', 'Male', 'Gandhi'),
+    (8, 'Kentucky - USA', 'Muhammad', 'Male', 'Ali'),
+    (10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+    (11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
+
