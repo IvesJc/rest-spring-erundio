99a40acfdcb34afe3afaa65849e3a9d299ee70bd
Ives Chiba <ivesjundi@gmail.com>
2024-04-27 16:14:12 -0300

    feat :sparkles: yaml media type implemented

--- rest-spring-erundio/src/main/java/br/com/erudio/config/WebConfig.java
+++ rest-spring-erundio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,12 +1,23 @@
 package br.com.erudio.config;
 
+import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
 
     //  this method configures how Spring handles content negotiation, specifying whether to use parameter-based negotiation, the parameter name to look for, whether to ignore the Accept header, default content type, and supported media types (e.g., JSON, XML).
     @Override
@@ -21,7 +32,9 @@ public class WebConfig implements WebMvcConfigurer {
                 //  "json" can be used as a parameter value in requests to indicate that the client wants JSON responses.
                 mediaType("json", MediaType.APPLICATION_JSON).
                 // "xml" as a parameter value to indicate that they want XML responses.
-                mediaType("xml", MediaType.APPLICATION_XML);
+                mediaType("xml", MediaType.APPLICATION_XML).
+                mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YAML);
+
 
         // Media Type via QUERY PARAM
         // http://localhost:8080/api/person/v1/1?mediaType=xml
--- rest-spring-erundio/src/main/java/br/com/erudio/controller/PersonController.java
+++ rest-spring-erundio/src/main/java/br/com/erudio/controller/PersonController.java
@@ -3,12 +3,13 @@ package br.com.erudio.controller;
 import br.com.erudio.dto.v1.PersonDTOv1;
 import br.com.erudio.dto.v2.PersonDTOv2;
 import br.com.erudio.services.PersonService;
+import br.com.erudio.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -17,41 +18,59 @@ public class PersonController {
     @Autowired
     private PersonService personServices;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
-                            MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YAML
+            })
     public List<PersonDTOv1> findAll(){
         return personServices.findAll();
     }
-    @GetMapping(value = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                        MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YAML
+            })
     public PersonDTOv1 findById(@PathVariable(value = "id") Long id){
         return personServices.findById(id);
     }
 
-    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
-                             MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YAML
+            })
     public PersonDTOv1 create(@RequestBody PersonDTOv1 person){
         return personServices.create(person);
     }
 
     // V2 with new attribute
-    @PostMapping(value = "/v2",
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                        MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(
+            value = "/v2",
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YAML
+            })
     public PersonDTOv2 createV2(@RequestBody PersonDTOv2 person){
         return personServices.createV2(person);
     }
 
-    @PutMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
-                            MediaType.APPLICATION_XML_VALUE})
+    @PutMapping(
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YAML
+            })
     public PersonDTOv1 update(@RequestBody PersonDTOv1 person){
         return personServices.update(person);
     }
 
-    @DeleteMapping(value = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                        MediaType.APPLICATION_XML_VALUE})
+    @DeleteMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YAML
+            })
     public ResponseEntity<?> deleteById(@PathVariable(value = "id") Long id){
         personServices.delete(id);
         return ResponseEntity.noContent().build();
