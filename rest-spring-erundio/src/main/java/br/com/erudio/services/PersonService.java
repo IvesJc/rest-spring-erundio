2d15ba82e44a8ab5c2a333424bfdb4d4576eebc2
Ives Chiba <ivesjundi@gmail.com>
2024-04-28 14:24:15 -0300

    feat :sparkles: implementing hateoas in every service method

--- rest-spring-erundio/src/main/java/br/com/erudio/services/PersonService.java
+++ rest-spring-erundio/src/main/java/br/com/erudio/services/PersonService.java
@@ -1,5 +1,6 @@
 package br.com.erudio.services;
 
+import br.com.erudio.controller.PersonController;
 import br.com.erudio.dto.v2.PersonDTOv2;
 import br.com.erudio.mapper.ModelMapper;
 import br.com.erudio.exceptions.ResourceNotFoundExecption;
@@ -8,6 +9,8 @@ import br.com.erudio.model.Person;
 import br.com.erudio.dto.v1.PersonDTOv1;
 import br.com.erudio.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,14 +29,22 @@ public class PersonService {
 
     public List<PersonDTOv1> findAll(){
         logger.info("Finding all people!");
-        return ModelMapper.parseListObjects(personRepository.findAll(), PersonDTOv1.class);
+        List<PersonDTOv1> dtos = ModelMapper.parseListObjects(personRepository.findAll(), PersonDTOv1.class);
+        dtos.
+            forEach(p -> p.add(linkTo(methodOn(PersonController.class).
+            findById(p.getKey())).
+            withSelfRel()));
+        return dtos;
     }
 
 
     public PersonDTOv1 findById(Long id){
         logger.info("Finding one person!");
         Person entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundExecption("No records found for this ID!"));
-        return ModelMapper.parseObject(entity, PersonDTOv1.class);
+
+        PersonDTOv1 dto = ModelMapper.parseObject(entity, PersonDTOv1.class);
+        dto.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return dto;
     }
 
 
@@ -42,10 +53,12 @@ public class PersonService {
         logger.info("Creating one person!");
         Person entity = ModelMapper.parseObject(personDTOv1, Person.class);
         // converte o DTO para a entidade       ^^^^^^^^^^^^^^^^^^^^^^^
-        var entidade = personRepository.save(entity);
-        return ModelMapper.parseObject(entidade, PersonDTOv1.class);
+        Person entidade = personRepository.save(entity);
+        PersonDTOv1 dto = ModelMapper.parseObject(entidade, PersonDTOv1.class);
         //                             ^^^^^^^^^^^^^^^^^^^^^^^^^
         // pra finalizar, converte entidade em DTO
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getKey())).withSelfRel());
+        return dto;
     }
     public PersonDTOv2 createV2(PersonDTOv2 personDTOv2){
         // recebe DTO       ^^^^^^^^^^^^^^^^^^^^
@@ -61,14 +74,16 @@ public class PersonService {
         logger.info("Updating one person!");
 
         Person entity =
-                personRepository.findById(personDTOv1.getId()).orElseThrow(() -> new ResourceNotFoundExecption(
+                personRepository.findById(personDTOv1.getKey()).orElseThrow(() -> new ResourceNotFoundExecption(
                         "No records found for this ID"));
         entity.setFirstName(personDTOv1.getFirstName());
         entity.setLastName((personDTOv1.getLastName()));
         entity.setAddress(personDTOv1.getAddress());
         entity.setGender(personDTOv1.getGender());
 
-        return ModelMapper.parseObject(personRepository.save(entity), PersonDTOv1.class);
+        PersonDTOv1 dto =ModelMapper.parseObject(personRepository.save(entity), PersonDTOv1.class);
+        dto.add(linkTo(methodOn(PersonController.class).findById(dto.getKey())).withSelfRel());
+        return dto;
     }
 
     public void delete(Long id){
