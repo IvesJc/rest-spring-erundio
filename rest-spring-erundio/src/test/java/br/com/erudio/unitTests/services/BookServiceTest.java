1b79989dc4dde43f13fcad78fc0cd1964857d35e
Ives Chiba <ivesjundi@gmail.com>
2024-04-30 19:54:41 -0300

    feat :sparkles: created, but not implementhed yet

---
+++ rest-spring-erundio/src/test/java/br/com/erudio/unitTests/services/BookServiceTest.java
@@ -0,0 +1,33 @@
+package br.com.erudio.unitTests.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
