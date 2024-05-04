a3037564012f2e691249274e19dec0c689b219ae
Ives Chiba <ivesjundi@gmail.com>
2024-05-04 16:17:54 -0300

    feat :sparkles: global cors

--- rest-spring-erundio/src/main/java/br/com/erudio/config/WebConfig.java
+++ rest-spring-erundio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.erudio.config;
 
 import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,9 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = corsOriginPatterns.split(",");
+        registry.
+                addMapping("/**").
+//                allowedMethods("GET", "POST", "PUT").
+                allowedMethods("*").
+                allowedOrigins(allowedOrigins).
+                allowCredentials(true);
     }
 
     //  this method configures how Spring handles content negotiation, specifying whether to use parameter-based negotiation, the parameter name to look for, whether to ignore the Accept header, default content type, and supported media types (e.g., JSON, XML).
