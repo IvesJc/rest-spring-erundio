672aeab29538e52e84d2e54d1ee7808ebe5d185b
Ives Chiba <ivesjundi@gmail.com>
2024-05-09 19:13:37 -0300

    feat :sparkles: new const

--- rest-spring-erundio/src/test/java/br/com/erudio/configs/TestConfigs.java
+++ rest-spring-erundio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -3,4 +3,9 @@ package br.com.erudio.configs;
 public class TestConfigs {
 
     public static final int SERVER_PORT = 8888;
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_YML = "application/x-yaml";
 }
