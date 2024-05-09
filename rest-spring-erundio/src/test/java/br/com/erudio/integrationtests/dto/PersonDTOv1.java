f79f068ce824dbc40a6bed5e05117954e27dc190
Ives Chiba <ivesjundi@gmail.com>
2024-05-09 19:16:25 -0300

    refactor :recycle: @JsonProperty

---
+++ rest-spring-erundio/src/test/java/br/com/erudio/integrationtests/dto/PersonDTOv1.java
@@ -0,0 +1,95 @@
+package br.com.erudio.integrationtests.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDTOv1 implements Serializable {
+
+    private Long id;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public PersonDTOv1() {
+    }
+
+    public PersonDTOv1(Long id, String firstName, String lastName, String address, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTOv1 that = (PersonDTOv1) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(address, that.address) && Objects.equals(gender, that.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDTOv1{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
