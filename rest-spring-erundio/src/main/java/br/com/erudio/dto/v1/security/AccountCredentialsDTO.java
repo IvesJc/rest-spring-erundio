bca034c49a84d5c3109500f15feaea91ac4b7040
Ives Chiba <ivesjundi@gmail.com>
2024-05-10 20:12:56 -0300

    feat :sparkles: DTO for token and account credentials

---
+++ rest-spring-erundio/src/main/java/br/com/erudio/dto/v1/security/AccountCredentialsDTO.java
@@ -0,0 +1,44 @@
+package br.com.erudio.dto.v1.security;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AccountCredentialsDTO implements Serializable {
+
+    private String userName;
+    private String password;
+
+    public AccountCredentialsDTO(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountCredentialsDTO that = (AccountCredentialsDTO) o;
+        return Objects.equals(userName, that.userName) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, password);
+    }
+}
---
+++ rest-spring-erundio/src/main/java/br/com/erudio/dto/v1/security/TokenDTO.java
@@ -0,0 +1,90 @@
+package br.com.erudio.dto.v1.security;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenDTO implements Serializable {
+
+    private String userName;
+    private Boolean authenticated;
+    private Date created;
+    private Date expiration;
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenDTO() {
+    }
+
+    public TokenDTO(String userName, Boolean authenticated, Date created, Date expiration, String accessToken, String refreshToken) {
+        this.userName = userName;
+        this.authenticated = authenticated;
+        this.created = created;
+        this.expiration = expiration;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Boolean getAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(Boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TokenDTO tokenDTO = (TokenDTO) o;
+        return Objects.equals(userName, tokenDTO.userName) && Objects.equals(authenticated, tokenDTO.authenticated) && Objects.equals(created, tokenDTO.created) && Objects.equals(expiration, tokenDTO.expiration) && Objects.equals(accessToken, tokenDTO.accessToken) && Objects.equals(refreshToken, tokenDTO.refreshToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, authenticated, created, expiration, accessToken, refreshToken);
+    }
+
+
+}
